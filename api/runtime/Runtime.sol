51095240d7829c13f558921e385f75c7f8ddf90f
laurentyzhang <laurent.y.zhang@gmail.com>
2023-07-16 17:22:26 -0600

    Removed exists() from runtime

--- api/runtime/Runtime.sol
+++ api/runtime/Runtime.sol
@@ -2,11 +2,6 @@
 pragma solidity ^0.8.19;
 
 contract Runtime {    
-    function exists(bytes memory key) public returns(bool) {
-        (,bytes memory id) = address(0xa0).call(abi.encodeWithSignature("exists(bytes)", key));
-        return abi.decode(id, (bool));
-    }
-
     function uuid() public returns(bytes memory args) {
         (,bytes memory id) = address(0xa0).call(abi.encodeWithSignature("uuid()"));     
         return id;
