08e0eaebc0d57e29d49607bd8210f88ba442a344
laurentyzhang <laurent.y.zhang@gmail.com>
2023-07-29 23:54:06 -0600

    Added the unit test for para vote

--- testsapps/vote_test.go
+++ testsapps/vote_test.go
@@ -5,44 +5,13 @@ import (
 	"path"
 	"path/filepath"
 	"testing"
-
-	evmcommon "github.com/arcology-network/evm/common"
-	eucommon "github.com/arcology-network/vm-adaptor/common"
-	"github.com/arcology-network/vm-adaptor/tests"
 )
 
-func TestVote(t *testing.T) {
+func TestParaVote(t *testing.T) {
 	currentPath, _ := os.Getwd()
-	targetPath := path.Join((path.Dir(filepath.Dir(currentPath))), "concurrentlib/")
-
-	contract, err := tests.NewContract(
-		[]evmcommon.Address{
-			eucommon.Coinbase,
-			eucommon.Alice,
-			eucommon.Bob,
-			eucommon.Abby,
-			eucommon.Abu,
-			eucommon.Andy,
-			eucommon.Anna,
-			eucommon.Antonio,
-			eucommon.Bailey,
-			eucommon.Baloo,
-			eucommon.Bambi,
-			eucommon.Banza,
-			eucommon.Beast,
-		},
-		eucommon.Alice,
-		targetPath, "examples/vote/vote_test.sol", "0.8.19", "SerialBallotCaller")
-
+	targetPath := path.Join(path.Dir(filepath.Dir(currentPath)), "concurrentlib/")
+	err, _, _ := DeployThenInvoke(targetPath, "examples/vote/vote_parallelized_mp_test.sol", "0.8.19", "ParaBallotCaller", "", []byte{}, false)
 	if err != nil {
-		return
-	}
-
-	if _, err = contract.Deploy(eucommon.Alice, 0); err != nil {
-		t.Error(err)
-	}
-
-	if _, err = contract.Invoke(eucommon.Alice, []byte{}); err != nil {
 		t.Error(err)
 	}
 }
